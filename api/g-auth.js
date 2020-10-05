556114501157e166bb83fd5c16fb3bbd99879901
Sasmini Alawathugoda <47610308+SasminiAlawathugoda@users.noreply.github.com>
2020-10-06 02:33:53 +0530

    Updated g-auth file

--- api/g-auth.js
+++ api/g-auth.js
@@ -1,3 +1,4 @@
+//Gmail Authorization
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 module.exports = (passport) => {
     passport.serializeUser((user, done) => {
@@ -7,8 +8,11 @@ module.exports = (passport) => {
         done(null, user);
     });
     passport.use(new GoogleStrategy({
+            //Replace with your clientID
             clientID: "558882333617-pm46tu0i8v5nsk283f8a151v7iodmnna.apps.googleusercontent.com",
+            //Replace with your clientSecret
             clientSecret:"TaD_3jV1kl_gj81syxIKVEWE",
+            //Replace with your callbackURL
             callbackURL: "http://localhost:3000/auth/google/callback"
         },
         (token, refreshToken, profile, done) => {

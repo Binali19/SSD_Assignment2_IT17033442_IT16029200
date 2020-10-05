4421501ceb9fb7732ac7c870dee41dee0c55698e
Sasmini Alawathugoda <47610308+SasminiAlawathugoda@users.noreply.github.com>
2020-10-06 02:13:05 +0530

    Created UIs

---
+++ ui/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm12 md12>
+            <v-container
+            fluid
+            grid-list-md
+            >
+            <v-layout row wrap>
+              <v-flex
+                md12
+                d-flex
+              >
+                <v-card flat tile class="mx-auto my-12">
+                  <v-img
+                    :src="require(`@/assets/images/fashion.jpg`)"
+                    :lazy-src="require(`@/assets/images/fashion.jpg`)"
+                  >
+                    <template v-slot:placeholder>
+                      <v-layout
+                        fill-height
+				   fill-width
+                        align-center
+                        justify-center
+                        ma-0
+                      >
+                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                      </v-layout>
+                    </template>
+                  </v-img>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
+</v-app>
+</template>
